72065bdd4c5a15b02bf7d3be57ad142d74752a3f
pedrojambo <pedrojambo@gmail.com>
2022-11-19 19:51:45 -0300

    19:51

---
+++ Flex/test2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//este programa demonstra como implementar uma operação de soma em C
+
+/*primeiro, 
+  atribuimos a x o valor 30
+  e a y o valor 40./* aaaa */
+  a seguir, apenas imprimimos o resultado!*/
+
+int main(){
+    int x = 30;
+    int y = 40;
+    printf("30 + 40 = %d\n",x+y);
+
+/* podemos perceber que o resultado esperado é 70*/
+
+    return 0;
+}
